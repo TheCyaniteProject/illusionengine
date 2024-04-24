7a4b382465d4d3025f907ee06d3ee4846b7f5faa
Nils Ramstöck <nilsramstoeck@gmail.com>
2024-04-24 12:29:52 +0200

    add comments

--- src/render/Api.ts
+++ src/render/Api.ts
@@ -1,5 +1,6 @@
 import type { Widget } from "@render/components/Widget";
 
+//Gross but it works ig x.x
 const types = typeof (0 as unknown);
 type Types = typeof types;
 type TypeToType<T extends Types> =
@@ -14,11 +15,23 @@ type TypeToType<T extends Types> =
   never;
 
 
-type ValidateDescriptor = { value: unknown, expected: Types; name: string; }[];
+type ValidateDescriptor = {value: unknown, expected: Types; name: string; }[];
 type Validated<T extends ValidateDescriptor> = {
   [index in keyof T]: TypeToType<T[index]['expected']>
 };
 
+/**
+ * Since the API methods are called from 3rd party code arguments must be validated.
+ * Example:
+ * ```ts
+ * const [x, y] = validateArguments([
+ *  { name: 'x', value: args[0], expected: "number" },
+ *  { name: 'y', value: args[1], expected: "number" },
+ * ] as const);
+ * ```
+ * The `as const` is needed to provide positional information of the argument types
+ * @returns Validated Argument Array
+ */
 function validateArguments<T extends ValidateDescriptor>(args: T): Validated<T> {
   for (const { value, name, expected } of args) {
     if (typeof value != expected) {
